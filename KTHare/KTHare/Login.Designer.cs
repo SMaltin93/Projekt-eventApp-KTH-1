47e13ef10fb3aeccd574c43ef1326c7520525379
maxisr <maxisr@kth.se>
2021-05-03 11:21:21 +0200

    Add Login-form

---
+++ KTHare/KTHare/Login.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace KTHare
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_mail = new System.Windows.Forms.TextBox();
+            this.lbl_mail = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(257, 158);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(101, 99);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(231, 23);
+            this.tb_password.TabIndex = 1;
+            // 
+            // tb_mail
+            // 
+            this.tb_mail.Location = new System.Drawing.Point(101, 44);
+            this.tb_mail.Name = "tb_mail";
+            this.tb_mail.Size = new System.Drawing.Size(231, 23);
+            this.tb_mail.TabIndex = 2;
+            // 
+            // lbl_mail
+            // 
+            this.lbl_mail.AutoSize = true;
+            this.lbl_mail.Location = new System.Drawing.Point(65, 47);
+            this.lbl_mail.Name = "lbl_mail";
+            this.lbl_mail.Size = new System.Drawing.Size(30, 15);
+            this.lbl_mail.TabIndex = 3;
+            this.lbl_mail.Text = "Mail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 202);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_mail);
+            this.Controls.Add(this.tb_mail);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tgbb;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_mail;
+        private System.Windows.Forms.Label lbl_mail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button o;
+        private System.Windows.Forms.Button s;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
